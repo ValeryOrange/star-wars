052d302200d15a44da948e2fc511196a0eab5b6b
Valerie Tokareva <tokareva@sports.ru>
2022-02-19 16:08:02 +0100

    created base methods to calculate velocity and coordinates

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,40 @@
-import { logger } from '../utilities/logger';
+import {
+    Position,
+    Velocity,
+} from './types';
 
-logger('Hello, server!');
+/**
+ * устанавливает двумерное значение скорости
+ * @param baseVelocity скорость по прямой
+ * @param angle угол поворота в градусах на декартовой плоскости
+ * @returns двумерное значение скорости, где x - скорость по вертикали,
+ *          y - по горизонтали
+ */
+function setVelocity2D(baseVelocity: number, angle: number): Velocity {
+    const angleRad = angle * Math.PI / 180;
+    const velocity = {
+        x: baseVelocity * Math.cos(angleRad),
+        y: baseVelocity * Math.sin(angleRad),
+    };
+    return velocity;
+}
+
+/**
+ * определяет значение координаты в конце движения
+ * @param initCoord начальная координата
+ * @param velocity скорость
+ * @param time время движения
+ * @returns координа
+ */
+function getCoord(initCoord: number, velocity: number, time: number): number {
+    return initCoord + velocity * time;
+}
+
+
+function move(initPos: Position, time: number, velocity: Velocity): Position {
+    const newCoord: Position = {...initPos};
+    for (const [key, value] of Object.entries(initPos)) {
+        newCoord[key] = getCoord(value, velocity[key], time);
+    }
+    return newCoord;
+}
---
+++ src/server/types.ts
@@ -0,0 +1,11 @@
+export type Position = {
+    x: number,
+    y: number,
+    [key: string]: number,
+};
+
+export type Velocity = {
+    x: number,
+    y: number,
+    [key: string]: number,
+};
